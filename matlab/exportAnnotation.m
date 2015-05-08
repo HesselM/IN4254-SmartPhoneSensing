7d1cd85b1ca508f6249b136e3c24b89fe0c8c9c0
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-08 15:39:40 +0200

    exportAnnotation: fixed name

--- matlab/exportAnnotation.m
+++ matlab/exportAnnotation.m
@@ -1,15 +1,20 @@
-function exportAnnotation(usedLog,t,r,a,m,xyz);
-% follow var-order of app:
+function exportAnnotation(usedLog,t,r,a,m,xyz)
+    %use name of old file to generate new filename
+    filename = usedLog(1:end-4); % remove '.txt'
+    filename = strcat(filename, '_corrected');
+    filename = strcat(filename, '.txt');
 
-%used name of old file to generate new filename
-filename = usedLog(1:end-4); % remove '.txt'
-filename = strcat(filename, '_corrected');
-filename = strcat(filename, '.txt');
-
-%open file
-fileID = fopen(filename,'w');
-fprintf(fileID,'timestamp,run,accuracy,motiontype,x,y,z\n');
-for i=1:size(t,1)
-    fprintf(fileID,'%d,%d,%d,%d,%.8f,%.8f,%.8f\n',t(i),r(i),a(i),m(i),xyz(i,1), xyz(i,2), xyz(i,3));
+    %open file
+    fileID = fopen(filename,'w');
+    
+    % follow var-order of app:
+    fprintf(fileID,'timestamp,run,accuracy,motiontype,x,y,z\n');
+    
+    %write all entries
+    for i=1:size(t,1)
+        fprintf(fileID,'%d,%d,%d,%d,%.8f,%.8f,%.8f\n',t(i),r(i),a(i),m(i),xyz(i,1), xyz(i,2), xyz(i,3));
+    end
+    
+    %close file
+    fclose(fileID);
 end
-fclose(fileID);
