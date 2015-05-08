15798916dde640af5f996dbf270964912a1ed5a9
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-08 15:09:03 +0200

    fixxannotiation: added boolean to show/hide pdfs

--- matlab/fixannotation.m
+++ matlab/fixannotation.m
@@ -1,4 +1,4 @@
-function [arr_motion] = fixannotation(run, astart, aend, type, arr_motion, arr_run, arr_magnitude, wsize)
+function [arr_motion] = fixannotation(run, astart, aend, type, arr_motion, arr_run, arr_magnitude, wsize, show_pdf)
 % run    = runID of sequence to be corrected
 % astart = start index of sequence (read from graph!) 
 %       -1 = ignore
@@ -11,6 +11,7 @@ function [arr_motion] = fixannotation(run, astart, aend, type, arr_motion, arr_r
 % arr_run    = run array of log
 % arr_magnitude = magnitude array determined from log
 % wsize         = windowsize for pdf standard deviation
+% show_pdf      = true/false, show pdf
 
 
 idx_start = 0;
@@ -50,65 +51,67 @@ plot(arr_motion_run, 'Color',[1,0,0]);
 legend('magnitude','motiontype');
 title('raw accel data ({m/s^2})', 'FontWeight','bold')
 
-% pdf
-% get & plot histograms
-[idle_h, idle_x] = getNormHist(round(arr_magnitude_run(arr_motion_run==1)*10));
-[walk_h, walk_x] = getNormHist(round(arr_magnitude_run(arr_motion_run==3)*10));
-[step_h, step_x] = getNormHist(round(arr_magnitude_run(arr_motion_run==4)*10));
+if (show_pdf)
+    % pdf
+    % get & plot histograms
+    [idle_h, idle_x] = getNormHist(round(arr_magnitude_run(arr_motion_run==1)*10));
+    [walk_h, walk_x] = getNormHist(round(arr_magnitude_run(arr_motion_run==3)*10));
+    [step_h, step_x] = getNormHist(round(arr_magnitude_run(arr_motion_run==4)*10));
 
-figure(2)
-clf
-hold on
-plot(idle_x/10, idle_h, 'Color',[1,0,0]);
-plot(walk_x/10, walk_h, 'Color',[0,1,0]);
-plot(step_x/10, step_h, 'Color',[0,0,1]);
-legend('idle','walk', 'step');
-title('pdf of raw accel data ({m/s^2})', 'FontWeight','bold')
+    figure(2)
+    clf
+    hold on
+    plot(idle_x/10, idle_h, 'Color',[1,0,0]);
+    plot(walk_x/10, walk_h, 'Color',[0,1,0]);
+    plot(step_x/10, step_h, 'Color',[0,0,1]);
+    legend('idle','walk', 'step');
+    title('pdf of raw accel data ({m/s^2})', 'FontWeight','bold')
 
-% pdf of sd of wsize samples
+    % pdf of sd of wsize samples
 
-% check if there are at least wsize
-std_idle = 0;
-std_walk = 0;
-std_step = 0;
-maxidx = size(arr_magnitude_run,1) - wsize;
-if (maxidx > 1)
-    for idx=1:maxidx
-        %determine motiontype which occures the most in the sampled window
-        data_window = arr_motion_run(idx:idx+wsize);
-        [h,x] = hist(data_window, unique(data_window));
-        mostType = x(h==max(h));
-        mostType = mostType(1); %choose first item when even.
-        
-        %calculate deviation over wsize samples
-        % and store deviation in correct array (walk/idle/step)
-        if (mostType == 1) %idle
-            std_idle(end+1) = std(arr_magnitude_run(idx:idx+wsize));
-        end
-        if (mostType == 3) %walk
-            std_walk(end+1) = std(arr_magnitude_run(idx:idx+wsize));
-        end
-        if (mostType == 4) %step
-            std_step(end+1) = std(arr_magnitude_run(idx:idx+wsize));
+    % check if there are at least wsize
+    std_idle = 0;
+    std_walk = 0;
+    std_step = 0;
+    maxidx = size(arr_magnitude_run,1) - wsize;
+    if (maxidx > 1)
+        for idx=1:maxidx
+            %determine motiontype which occures the most in the sampled window
+            data_window = arr_motion_run(idx:idx+wsize);
+            [h,x] = hist(data_window, unique(data_window));
+            mostType = x(h==max(h));
+            mostType = mostType(1); %choose first item when even.
+            
+            %calculate deviation over wsize samples
+            % and store deviation in correct array (walk/idle/step)
+            if (mostType == 1) %idle
+                std_idle(end+1) = std(arr_magnitude_run(idx:idx+wsize));
+            end
+            if (mostType == 3) %walk
+                std_walk(end+1) = std(arr_magnitude_run(idx:idx+wsize));
+            end
+            if (mostType == 4) %step
+                std_step(end+1) = std(arr_magnitude_run(idx:idx+wsize));
+            end
         end
     end
-end
 
-%calculat histogram
-% get & plot histograms
-[std_idle_h, std_idle_x] = getNormHist(round(std_idle*10));
-[std_walk_h, std_walk_x] = getNormHist(round(std_walk*10));
-[std_step_h, std_step_x] = getNormHist(round(std_step*10));
+    %calculat histogram
+    % get & plot histograms
+    [std_idle_h, std_idle_x] = getNormHist(round(std_idle*10));
+    [std_walk_h, std_walk_x] = getNormHist(round(std_walk*10));
+    [std_step_h, std_step_x] = getNormHist(round(std_step*10));
 
-figure(3)
-clf
-hold on
-plot(std_idle_x/10, std_idle_h, 'Color',[1,0,0]);
-plot(std_walk_x/10, std_walk_h, 'Color',[0,1,0]);
-plot(std_step_x/10, std_step_h, 'Color',[0,0,1]);
-legend('idle','walk', 'step');
-title('pdf of std of raw accel data ({m/s^2})', 'FontWeight','bold')
+    figure(3)
+    clf
+    hold on
+    plot(std_idle_x/10, std_idle_h, 'Color',[1,0,0]);
+    plot(std_walk_x/10, std_walk_h, 'Color',[0,1,0]);
+    plot(std_step_x/10, std_step_h, 'Color',[0,0,1]);
+    legend('idle','walk', 'step');
+    title('pdf of std of raw accel data ({m/s^2})', 'FontWeight','bold')
 
+    end
 end
 
 function [mag_hist_pdf, mag_x] = getNormHist(mag)
