6bab03889abe35ad3a0c56f2b5ef180a366c53f6
jbmiog <jeffrey.miog@gmail.com>
2015-05-08 02:46:07 +0200

    autcor finished, kladversie in report, added many graphs

--- matlab/auto_cor.m
+++ matlab/auto_cor.m
@@ -1,9 +1,9 @@
-function x = auto_cor(sign,m,t)	
+function x = auto_cor(signal,m,t)	
     y = 0;
-    mean1 = mean(sign(m:m+t));
-    mean2 = mean(sign(m+t:m+t+t));
+    mean1 = mean(signal(m:m+t-1));
+    mean2 = mean(signal(m+t):m+t+t-1);
 	for k = 0:t-1
-		y = y + (sign(m+k)-mean1 )*(sign(m+k+t)-mean2);
+		y = y + (signal(m+k)-mean1)*(signal(m+k+t)-mean2);
 	end  
-     x = y / (t*std(sign(m:m+t))* std(sign(m+t:m+t+t)));
+     x = y / (t*std(signal(m:m+t-1)) * std(signal(m+t:m+t+t-1)));
 end
--- matlab/autocorrelation.m
+++ matlab/autocorrelation.m
@@ -29,7 +29,7 @@ if (size(timestamps,1) > 1000)
 end
 
 % calculate magnitude
-magnitude   = sqrt(sum(accel.^2, 2));
+magnitude   = sqrt(sum(accel.^2,2));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % RAW DATA PLOT
@@ -45,19 +45,35 @@ legend('accel','motiontype','magnitude');
 title('raw accel data ({m/s^2})', 'FontWeight','bold')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% RAW DATA PLOT OF ONLY WALKING
+% crunching data to magnitudes of corresponding motiontype
+
+%walking
+%with filter now
+
 accel_walk = accel(motiontype==3,:);
 magnitudeWalk = sqrt(sum(accel_walk.^2,2));
 motiontype_walk = motiontype(motiontype==3);
+%filter num => use fdatool in command window
+% 30 hz, cutuff = 250
+motiontype_walk = conv(motiontype_walk,Num);
 
-%plot this data
+% idle
+accel_idle = accel(motiontype==1,:);
+magnitudeIdle = sqrt(sum(accel_idle.^2,2));
+motiontype_idle = motiontype(motiontype==1);
+%filter 
+motiontype_idle = conv(motiontype_idle,Num);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CALCULATE STANDARD  DEVIATION OF MAGNITUDE
+
 figure(2)
-clf
 hold on
-plot(magnitudeWalk);
-plot(motiontype_walk);
-legend('walk magnitude','motiontype');
-title('magnitude of walking');
+h = histogram(magnitudeWalk,'Normalization','probability');
+p = histogram(magnitudeIdle,'Normalization','probability');
+legend('walk,idle');
+title('standard deviation in magnitude of acceleration(m/s^2)');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % CALCUTE AUTOCORRELATION 
@@ -65,39 +81,40 @@ title('magnitude of walking');
 % m = samplenumber;
 %  t = lag;
 %  k = 0;
-
-magLength = length(magnitudeWalk);
-
-m_offset = 600;
-m_max = 1000;
-
-t_min = 70;
+%
+m_offset = 1;
+m_max = length(magnitudeWalk)-200;
+t_min = 40;
 t_max = 100;
-
-y = zeros(m_max, 1);
+y_walk = zeros(m_max, 1);
 
 for m = 1:m_max
     for t = t_min:t_max
        cor = auto_cor(magnitudeWalk, m+m_offset, t);
-       y(m) = max(cor, y(m));     
+       y_walk(m) = max(cor, y_walk(m));     
     end
 end
 
+m_offset = 1;
+m_max = length(magnitudeIdle)-200;
+t_min = 40;
+t_max = 100;
 
+y_idle = zeros(m_max, 1);
 
-%delete all zero entrees, prototyping all samples takes too long
-y(y==0) = [];
-disp(y);
+for m = 1:m_max
+    for t = t_min:t_max
+       cor = auto_cor(magnitudeIdle, m+m_offset, t);
+       y_idle(m) = max(cor, y_idle(m));     
+    end
+end
 
-disp(max(y));    
-yround = round(y*100);
-
-[yhist,xvalues] = hist(yround,unique(yround));
-yhist = yhist ./ sum(yhist);
-
-figure(3)
-clf
-plot(xvalues./100,yhist);
-legend('y');
-title('pdf y');
+%plot data itsel
+figure(4)
+hold on
+nbins = 20;
+h = histogram(y_walk,nbins,'Normalization','probability');
+p = histogram(y_idle,nbins,'Normalization','probability');
+legend('walk','idle');
+title('maximum normalized auto-correlation');
 
