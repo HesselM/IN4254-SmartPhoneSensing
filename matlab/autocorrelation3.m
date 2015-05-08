a6cdba42663eced499ada7a5c3320a7793797b6a
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-08 12:37:31 +0200

    autocor3: more optimisiations

--- matlab/autocorrelation3.m
+++ matlab/autocorrelation3.m
@@ -25,49 +25,39 @@ end
 
 
 function x = auto_cor2(a,m,t)	
-    %matrix: represents m+k, for all m-samples and all k=0...k=t+1 values
-    m_k = arrayfun(@colon, m, m+t-1, 'Uniform', false);
-    m_k = vertcat(m_k{:})';
+    %matrix: represents index m+k, for all m-samples and all k=0...k=t+1 values
+    m_k = coloncatrld(m, m+t-1);
 
-
-    % sliding windows
+    % SETTING SLIDING WINDOWS
 
     % start:m, width:t
     %matrix: represents all indices for samples from index m..m+t-1
-    m_t = arrayfun(@colon, m, m+t-1, 'Uniform', false);
-    m_t = vertcat(m_t{:})';
-
+    %m_t = coloncatrld(m, m+t-1);
+    % ==> it turns out m_t equals m_k, when matrices are used
+    m_t = m_k;
+              
     % start:m+t, width:t
     %matrix: represents all indices for samples from index m+t..m+t+t-1
-    mt_t = arrayfun(@colon, m+t, m+t+t-1, 'Uniform', false);
-    mt_t = vertcat(mt_t{:})';
-    % autocor: sum
-
+    mt_t = coloncatrld(m+t, m+t+t-1);
+              
+    % MEAN CALCULATIONS
     %mean of sliding window for sample m, in range m..m+t-1
     % size: 1*size(m,1)
+    %  mean needs to be subtracted from all signal values a(m+k):
+    %   matrix a(m_k) = t*size(m,1);  
+    %   so we need to repeat the mean t-times    mean_m_t = mean(a(m_t));
+              
     mean_m_t = mean(a(m_t));
-    
-    %mean needs to be subtracted from all signal values a(m+k):
-    % matrix a(m_k) = t*size(m,1);  
-    % so we need to repeat the mean t-times
     mean_m_t = mean_m_t(ones(1,t),:);
     
     %mean of sliding window for sample m, in range m+t..m+t+t-1
     mean_mt_t = mean(a(mt_t));
-    % matrix a(m_k+t) = t*size(m,1);  
-    % so we need to repeat the mean t-times
     mean_mt_t = mean_mt_t(ones(1,t),:);
     
-    %differences 
-    diff1 = a(m_k) - mean_m_t;
-    diff2 = a(m_k+t) - mean_mt_t;
-
-    %mutiplication
-    mult = diff1 .* diff2;
-
-    %sum
-    s    = sum(mult);
+    % EXPECTED VALUE
+    s = sum( (a(m_k) - mean_m_t) .* (a(m_k+t) - mean_mt_t) );
       
+    % NORMALISATION (?) 
     % autocor: sum / variation
     x = s ./ (t * std(a(m_t)) .* std(a(mt_t)));
 end
@@ -77,4 +67,36 @@ function [h, x] = getNormHist(signal)
     [h, x] = hist(signal,unique(signal));
     %normalize
     h  = h  ./ sum(h);
+end
+
+% source: http://blogs.mathworks.com/loren/2008/10/13/vectorizing-the-notion-of-colon/
+function x = coloncatrld(start, stop)
+    % COLONCAT Concatenate colon expressions
+    %    X = COLONCAT(START,STOP) returns a vector containing the values
+    %    [START(1):STOP(1) START(2):STOP(2) START(END):STOP(END)].
+
+    % Based on Peter Acklam's code for run length decoding.
+    len = stop - start + 1;
+      
+    % keep only sequences whose length is positive
+    pos = len > 0;
+    start = start(pos);
+    stop = stop(pos);
+    len = len(pos);
+    if isempty(len)
+        x = [];
+    return;
+    end
+      
+    % expand out the colon expressions
+    endlocs = cumsum(len);  
+    incr = ones(1, endlocs(end));  
+    jumps = start(2:end) - stop(1:end-1);  
+    incr(endlocs(1:end-1)+1) = jumps;
+    incr(1) = start(1);
+    x = cumsum(incr);
+              
+    rows = size(start,2);
+    cols = size(x,2)/rows;
+    x = reshape(x, cols, rows);
 end
