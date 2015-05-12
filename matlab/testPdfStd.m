e3a70d569b0e02d40f9de6520cb4031d50b8fa88
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-12 13:37:17 +0200

    testPdfStd: added all testcases

--- matlab/testPdfStd.m
+++ matlab/testPdfStd.m
@@ -1,10 +1,24 @@
 %test different sizes
 
-function testPdfStd(run, motiontype, magnitude)
+function r = testPdfStd(run, motiontype, magnitude)
     binacc = 10;
-    testPdfStdw(10, run, motiontype, magnitude, binacc)
-    testPdfStdw(20, run, motiontype, magnitude, binacc)
-    testPdfStdw(30, run, motiontype, magnitude, binacc)
+    r = zeros(16, 10);
+    r( 1,:) = testPdfStdw( 10, run, motiontype, magnitude, binacc);
+    r( 2,:) = testPdfStdw( 20, run, motiontype, magnitude, binacc);
+    r( 3,:) = testPdfStdw( 30, run, motiontype, magnitude, binacc);
+    r( 4,:) = testPdfStdw( 40, run, motiontype, magnitude, binacc);
+    r( 5,:) = testPdfStdw( 50, run, motiontype, magnitude, binacc);
+    r( 6,:) = testPdfStdw( 60, run, motiontype, magnitude, binacc);
+    r( 7,:) = testPdfStdw( 70, run, motiontype, magnitude, binacc);
+    r( 8,:) = testPdfStdw( 80, run, motiontype, magnitude, binacc);
+    r( 9,:) = testPdfStdw( 90, run, motiontype, magnitude, binacc);
+    r(10,:) = testPdfStdw(100, run, motiontype, magnitude, binacc);
+    r(11,:) = testPdfStdw(125, run, motiontype, magnitude, binacc);
+    r(12,:) = testPdfStdw(150, run, motiontype, magnitude, binacc);
+    r(13,:) = testPdfStdw(175, run, motiontype, magnitude, binacc);
+    r(14,:) = testPdfStdw(200, run, motiontype, magnitude, binacc);
+    r(15,:) = testPdfStdw(225, run, motiontype, magnitude, binacc);
+    r(16,:) = testPdfStdw(250, run, motiontype, magnitude, binacc);
 end
 
 function r = testPdfStdw(wsize, run, motiontype, magnitude, binacc)
