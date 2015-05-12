9280d29d3e4bbec54d053c13f1fd7b4af501b80d
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-12 13:18:08 +0200

    testPdfStd: revamped structure to test different wsize

--- matlab/testPdfStd.m
+++ matlab/testPdfStd.m
@@ -1,22 +1,14 @@
 %test different sizes
 
-function testPdfStd
-testPDFSTDw(10);
-testPDFSTDw(20);
-testPDFSTDw(30);
+function testPdfStd(run, motiontype, magnitude)
+    binacc = 10;
+    testPdfStdw(10, run, motiontype, magnitude, binacc)
+    testPdfStdw(20, run, motiontype, magnitude, binacc)
+    testPdfStdw(30, run, motiontype, magnitude, binacc)
 end
 
-function testPDFSTDw(wsize)
-windowsize = wsize;
-plotPDFSTD;
-figure(wsize);clf;hold on
-plot( hist_x_std_idle, hist_y_std_idle, 'Color',[1,0,0]);
-plot( hist_x_std_walk, hist_y_std_walk, 'Color',[0,1,0]);
-plot( hist_x_std_step, hist_y_std_step, 'Color',[0,0,1]);
-legend('idle','walk', 'qstep');
-t = 'pdf of std(magnitude), size=';
-t = strcat(t, wsize);
-t = strcat(t, ' ({m/s^2})');
-title(t, 'FontWeight','bold')
+function r = testPdfStdw(wsize, run, motiontype, magnitude, binacc)
+    [hi, xi, hs, xs, hw, xw] = calcPdfStd(wsize, run, motiontype, magnitude, binacc);
+    m = compHist(hi, xi, hs, xs, hw, xw);
+    r = [wsize m(:,1)' m(:,2)' m(:,3)'];
 end
-
