c6d2fae9bed79a97ff2888a479adb901b10d7e32
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-13 13:28:59 +0200

    getNormHist: clean up

--- matlab/getNormHist.m
+++ matlab/getNormHist.m
@@ -1,6 +1,6 @@
 function [h, x] = getNormHist(signal)
     %calculate histogram
-    [h, x] = hist(signal,unique(signal));
+    [h, x] = hist(signal,50);
     %normalize
     h  = h  ./ sum(h);
 end
