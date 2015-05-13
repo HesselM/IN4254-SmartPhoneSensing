b51ecb4a41721f1ceca29774ad6ef62022bbee55
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-13 13:23:15 +0200

    calcPdf: clean up

--- matlab/calcPdf.m
+++ matlab/calcPdf.m
@@ -1,27 +1,16 @@
-function [hi, xi, hs, xs, hw, xw] = calcPdf(motiontype, magnitude, binacc)
+function [hi, xi, hs, xs, hw, xw] = calcPdf(m, signal, binacc)
 
 
 %mutiplication by 10 is done for bin-purposes, 
 %   to get the histogram accurate at 0.1
 
 
-[hi, xi] = getNormHist(round(magnitude(motiontype==1)*binacc));
-[hw, xw] = getNormHist(round(magnitude(motiontype==3)*binacc));
-[hs, xs] = getNormHist(round(magnitude(motiontype==4)*binacc));
+[hi, xi] = getNormHist(round(signal(m==1)*binacc));
+[hw, xw] = getNormHist(round(signal(m==3)*binacc));
+[hs, xs] = getNormHist(round(signal(m==4)*binacc));
 
 xi = xi / binacc;
 xs = xs / binacc;
 xw = xw / binacc;
 
 end
-%{
-
-figure(2)
-clf
-hold on
-plot( xi, hi, 'Color',[1,0,0]);
-plot( xw, hw, 'Color',[0,1,0]);
-plot( xs, hs, 'Color',[0,0,1]);
-legend('idle','walk', 'qstep');
-title('pdf of magnitude ({m/s^2})', 'FontWeight','bold')
-    %}
