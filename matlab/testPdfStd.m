007a2f8da6089c9b147f759072e8e2e57295515b
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-15 11:19:34 +0200

    testPdfStd: replaced calcPdfStd with calcStd and included normalisation

--- matlab/testPdfStd.m
+++ matlab/testPdfStd.m
@@ -1,15 +1,26 @@
 %test different sizes
 
-function r = testPdfStd(wsize, r, m, signal)
+function result = testPdfStd(wsize, r, m, signal)
     binacc = 10;
-    r = zeros(size(wsize,2), 10);
+    result = zeros(size(wsize,2), 10);
     for w=1:size(wsize,2)
-        r(w,:) = testPdfStdw( wsize(w), r, m, signal, binacc);
+        result(w,:) = testPdfStdw( wsize(w), r, m, signal, binacc);
     end
 end
 
-function r = testPdfStdw(wsize, r, m, signal, binacc)
-    [hi, xi, hs, xs, hw, xw] = calcPdfStd(wsize, r, m, signal, binacc);
+function r = testPdfStdw(wsize, run, m, signal, binacc)
+    %generate std-data
+    [stdi, stds, stdw] = calcStd(wsize, run, m, signal);
+    
+    %create pdf of data
+    [hi, xi] = getNormHist(round(stdi*binacc));
+    [hw, xw] = getNormHist(round(stdw*binacc));
+    [hs, xs] = getNormHist(round(stds*binacc));
+    %xi = xi / binacc;
+    %xw = xw / binacc;
+    %xs = xs / binacc;
+    
+    %compare pdf
     m = compPdf(hi, xi, hs, xs, hw, xw);
     r = [wsize m(:,1)' m(:,2)' m(:,3)'];
 end
