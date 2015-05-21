044f0c4364148e35860d77800528d0fded5c3128
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-21 09:15:56 +0200

    translate an signal from log to features for classification

---
+++ matlab/signal2feature.m
@@ -0,0 +1,47 @@
+function f = signal2feature(accel, wstd, wmean, tmin, tmax)
+    %number of elements in accel
+    maxidx = size(accel,1);
+
+    % set maximum window for all calculations
+    max_std  = maxidx - wstd;
+    max_mean = maxidx - wmean;
+    max_nasc = maxidx - 2*tmax;
+    maxidx   = min(max_std, min(max_mean, max_nasc));
+
+    %init features vector
+    f = zeros(maxidx,3);
+    
+    for i=1:maxidx
+        %Standard Deviation of magnitude
+        mag_std = std(sqrt(sum(accel(i:i+wstd).^2, 2)));
+
+        %Mean of Heading
+        a1   = accel(i+0:i-1+wmean,:);
+        a2   = accel(i+1:i+0+wmean,:);
+        magh = mean(sqrt(sum((a2 - a1).^2, 2)));
+
+        %NASC
+        cor1 = nasc(i, accel(:,1), tmin, tmax);
+        cor2 = nasc(i, accel(:,2), tmin, tmax);
+        cor3 = nasc(i, accel(:,3), tmin, tmax);
+        cor  = max(cor1, max(cor2, cor3));
+
+        %add to feature vector
+        f(i,:) = [mag_std, magh, cor];
+    end
+end
+
+function cor = nasc(m, a, tmin, tmax)
+    cor = 0;
+    for t=tmin:tmax
+        s = 0;
+        mt      = m:m+t-1;
+        mtt     = mt + t;
+        meanmt  = mean(a(mt));
+        meanmtt = mean(a(mtt));
+        for k = 0:t-1
+            s = s + ((a(m+k) - meanmt) * (a(m+k+t) - meanmtt));
+        end
+        cor = max(cor, s / (t*std(a(mt))*std(a(mtt))));
+    end
+end
