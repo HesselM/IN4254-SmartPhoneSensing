20e54a69948e80a0ff7f078774823d6189b230f5
Hessel van der Molen <hmolen.science@gmail.com>
2015-05-22 14:48:10 +0200

    sig2feature: added process tracking

--- matlab/signal2feature.m
+++ matlab/signal2feature.m
@@ -12,8 +12,13 @@ function f = signal2feature(accel, wstd, wmean, tmin, tmax)
     f = zeros(maxidx,3);
     
     for i=1:maxidx
+
+        if (mod(i,100) == 0)
+            disp(sprintf('i:%d/%d',i,maxidx));
+        end
+
         %Standard Deviation of magnitude
-        mag_std = std(sqrt(sum(accel(i:i+wstd).^2, 2)));
+        mag_std = std(sqrt(sum(accel(i:i+wstd,:).^2, 2)));
 
         %Mean of Heading
         a1   = accel(i+0:i-1+wmean,:);
@@ -21,9 +26,9 @@ function f = signal2feature(accel, wstd, wmean, tmin, tmax)
         magh = mean(sqrt(sum((a2 - a1).^2, 2)));
 
         %NASC
-        cor1 = nasc(i, accel(:,1), tmin, tmax);
-        cor2 = nasc(i, accel(:,2), tmin, tmax);
-        cor3 = nasc(i, accel(:,3), tmin, tmax);
+        [cor1,t1] = nasc(i, accel(:,1), tmin, tmax);
+        [cor2,t2] = nasc(i, accel(:,2), tmin, tmax);
+        [cor3,t3] = nasc(i, accel(:,3), tmin, tmax);
         cor  = max(cor1, max(cor2, cor3));
 
         %add to feature vector
@@ -31,8 +36,9 @@ function f = signal2feature(accel, wstd, wmean, tmin, tmax)
     end
 end
 
-function cor = nasc(m, a, tmin, tmax)
-    cor = 0;
+function [cor tcor] = nasc(m, a, tmin, tmax)
+    cor  = 0;
+    tcor = 0;
     for t=tmin:tmax
         s = 0;
         mt      = m:m+t-1;
@@ -42,6 +48,12 @@ function cor = nasc(m, a, tmin, tmax)
         for k = 0:t-1
             s = s + ((a(m+k) - meanmt) * (a(m+k+t) - meanmtt));
         end
-        cor = max(cor, s / (t*std(a(mt))*std(a(mtt))));
+
+        chi = s / (t*std(a(mt))*std(a(mtt)));
+
+        if (chi > cor)
+            cor  = chi;
+            tcor = t; 
+        end
     end
 end
